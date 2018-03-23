35ca8a4c22df71f79862975496ea92ffe1ee4933
Alex Timofeev <atimofeev@reactant.ru>
2018-03-23 10:31:42 +0700

    Extend Competition spec

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,13 +2,15 @@ class Competition < ActiveRecord::Base
   has_many :entries, inverse_of: :competition
   validates_presence_of :name
 
-  def get_list(attribute = :id)
-    get_lists.select { |l| l['name'] == name }.first[attribute.to_s]
-  end
-
   def list_id
     update_column(:mailchimp_list_id, get_list(:id)) unless mailchimp_list_id.present?
     mailchimp_list_id
+  end
+
+  def get_list(attribute = :id)
+    lists = get_lists.select { |l| l['name'] == name }
+    raise NameError, "A list named `#{name}` doesn't exist on MailChimp side yet" unless lists.any?
+    lists.first[attribute.to_s]
   end
 
   private
--- spec/models/competitions_spec.rb
+++ spec/models/competitions_spec.rb
@@ -1,7 +1,43 @@
 require "rails_helper"
 
 describe Competition do
-  it "should require a name" do
-    expect { Competition.create!(name: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+  fixtures :competitions
+
+  context 'validations' do
+    it "should require a name" do
+      expect { Competition.create!(name: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  context 'MailChimp' do
+    describe '#list_id' do
+      subject { competition.list_id }
+
+      context 'when associated list does not exit yet' do
+        let(:competition) { competitions(:competition_inexistent) }
+        it 'raises an execption' do
+          expect { subject }.to raise_error(NameError, "A list named `#{competition.name}` doesn't exist on MailChimp side yet")
+        end
+      end
+
+      context 'when associated list exists' do
+        let(:competition) { competitions(:competition) }
+
+        it 'updates competition list id' do
+          expect { subject }.to change { competition.mailchimp_list_id }.from(nil).to('6a59ad935f')
+        end
+        it 'returns list id' do
+          expect(subject).to eq '6a59ad935f'
+        end
+      end
+    end
+
+    describe '#get_list' do
+      let(:competition) { competitions(:competition) }
+
+      it 'returns list attributes' do
+        expect(competition.get_list(:name)).to eq competition.name
+      end
+    end
   end
 end
