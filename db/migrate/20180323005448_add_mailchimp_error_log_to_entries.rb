bb894e396a50590047ec6421b21b0466a9922c2d
Alex Timofeev <atimofeev@reactant.ru>
2018-04-02 03:06:30 +0700

    Apply feedback based changes

--- db/migrate/20180323005448_add_mailchimp_error_log_to_entries.rb
+++ db/migrate/20180323005448_add_mailchimp_error_log_to_entries.rb
@@ -1,6 +1,5 @@
 class AddMailchimpErrorLogToEntries < ActiveRecord::Migration
   def change
     add_column :entries, :mailchimp_error_log, :jsonb, null: false, default: '{}'
-    add_index :entries, :mailchimp_error_log, using: :gin
   end
 end
